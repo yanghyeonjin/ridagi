9864ec5db98a85e21bf3dcbbe05adaee1250f55f
yanghyeonjin <guswls7081@gmail.com>
2021-04-24 12:51:18 +0900

    - Context API

---
+++ context-tutorial/src/components/SelectColors.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+import { ColorConsumer } from "contexts/color";
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+const SelectColors = () => {
+  return (
+    <div>
+      <h2>색상을 선택하세요.</h2>
+      <ColorConsumer>
+        {({ actions }) => (
+          <div
+            style={{
+              display: "flex",
+              marginBottom: "24px",
+              paddingBottom: "24px",
+              borderBottom: "1px solid black",
+            }}
+          >
+            {colors.map((color) => (
+              <div
+                key={color}
+                style={{
+                  background: color,
+                  width: "24px",
+                  height: "24px",
+                  cursor: "pointer",
+                }}
+                onClick={() => actions.setColor(color)}
+                onContextMenu={(e) => {
+                  e.preventDefault(); // 마우스 오른쪽 버튼 클릭 시 메뉴가 뜨는 것을 무시함
+                  actions.setSubcolor(color);
+                }}
+              ></div>
+            ))}
+          </div>
+        )}
+      </ColorConsumer>
+    </div>
+  );
+};
+
+export default SelectColors;
